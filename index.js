7b06d5ad4837d4d5e79733d9f1f6394149e4e3f4
Aviral Tiwari <tiwariaviral580@gmail.com>
2024-11-01 22:29:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get ("/",(req,res )=>{
+    res.render('index.ejs');
+});
+
+app.post("/submit",(req,res)=>{
+    res.render('contact.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
